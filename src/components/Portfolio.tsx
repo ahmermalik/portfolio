0c869a1fc789c7efd9127924463cf83137fbb079
Ahmer Malik <ahmermalik@Ahmers-Mac-mini.local>
2023-09-14 22:24:11 -0500

    implement portfolio section, still need to improve responsiveness, but styling is done for now

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -7,11 +7,11 @@ import Box from "@mui/material/Box";
 const Portfolio: React.FC = () => {
   const projects = [
     {
-      name: "Shish Kabob Cafe",
-      technologies: ["JavaScript", "Google Analytics", "jQuery"],
+      name: "Najumi",
+      technologies: ["Typescript", "Google Analytics", "jQuery"],
       description:
-        "Shish Kabob Cafe is a local restaurant known for its mouthwatering Mediterranean cuisine. To enhance its online presence and streamline the ordering process, we developed a mobile-first, responsive web application and integrated a seamless delivery system.",
-      image: "/image/path",
+        "Spearheaded the launch of three pivotal product features, achieving an 18% increase in subscription rates by crypto traders.",
+      image: "/images/projects/najumi.png",
       url: "github.com/projecturl",
       summary:
         "This project leverages JavaScript, Google Analytics, and jQuery to provide an exceptional user experience while boosting the restaurant's delivery service.",
@@ -21,6 +21,7 @@ const Portfolio: React.FC = () => {
         "Interactive Menu",
       ],
     },
+
     {
       name: "CAI Storefront",
       technologies: [
@@ -35,7 +36,7 @@ const Portfolio: React.FC = () => {
       ],
       description:
         "Shish Kabob Cafe is a local restaurant known for its mouthwatering Mediterranean cuisine. To enhance its online presence and streamline the ordering process, we developed a mobile-first, responsive web application and integrated a seamless delivery system.",
-      image: "/image/path",
+      image: "/images/projects/caistore.png",
       url: "github.com/projecturl",
       summary:
         "This project leverages JavaScript, Google Analytics, and jQuery to provide an exceptional user experience while boosting the restaurant's delivery service.",
@@ -46,11 +47,11 @@ const Portfolio: React.FC = () => {
       ],
     },
     {
-      name: "Najumi",
-      technologies: ["Typescript", "Google Analytics", "jQuery"],
+      name: "Shish Kabob Cafe",
+      technologies: ["JavaScript", "Google Analytics", "jQuery"],
       description:
-        "Spearheaded the launch of three pivotal product features, achieving an 18% increase in subscription rates by crypto traders.",
-      image: "/image/path",
+        "Shish Kabob Cafe is a local restaurant known for its mouthwatering Mediterranean cuisine. To enhance its online presence and streamline the ordering process, we developed a mobile-first, responsive web application and integrated a seamless delivery system.",
+      image: "/images/projects/shishkabob.png",
       url: "github.com/projecturl",
       summary:
         "This project leverages JavaScript, Google Analytics, and jQuery to provide an exceptional user experience while boosting the restaurant's delivery service.",
@@ -65,7 +66,7 @@ const Portfolio: React.FC = () => {
       technologies: ["React", "Google Analytics", "jQuery"],
       description:
         "Shish Kabob Cafe is a local restaurant known for its mouthwatering Mediterranean cuisine. To enhance its online presence and streamline the ordering process, we developed a mobile-first, responsive web application and integrated a seamless delivery system.",
-      image: "/image/path",
+      image: "/images/projects/chatapp2.png",
       url: "github.com/projecturl",
       summary:
         "This project leverages JavaScript, Google Analytics, and jQuery to provide an exceptional user experience while boosting the restaurant's delivery service.",
@@ -80,7 +81,7 @@ const Portfolio: React.FC = () => {
       technologies: ["Python", "Google Analytics", "jQuery"],
       description:
         "Shish Kabob Cafe is a local restaurant known for its mouthwatering Mediterranean cuisine. To enhance its online presence and streamline the ordering process, we developed a mobile-first, responsive web application and integrated a seamless delivery system.",
-      image: "/image/path",
+      image: "/images/projects/cryptotistics.png",
       url: "github.com/projecturl",
       summary:
         "This project leverages JavaScript, Google Analytics, and jQuery to provide an exceptional user experience while boosting the restaurant's delivery service.",
@@ -95,7 +96,7 @@ const Portfolio: React.FC = () => {
       technologies: ["JavaScript", "Google Analytics", "jQuery"],
       description:
         "Shish Kabob Cafe is a local restaurant known for its mouthwatering Mediterranean cuisine. To enhance its online presence and streamline the ordering process, we developed a mobile-first, responsive web application and integrated a seamless delivery system.",
-      image: "/image/path",
+      image: "/images/projects/bullbearbuddies.png",
       url: "github.com/projecturl",
       summary:
         "This project leverages JavaScript, Google Analytics, and jQuery to provide an exceptional user experience while boosting the restaurant's delivery service.",
@@ -110,7 +111,7 @@ const Portfolio: React.FC = () => {
       technologies: ["Next.js", "Mix Panel", "jQuery"],
       description:
         "Shish Kabob Cafe is a local restaurant known for its mouthwatering Mediterranean cuisine. To enhance its online presence and streamline the ordering process, we developed a mobile-first, responsive web application and integrated a seamless delivery system.",
-      image: "/image/path",
+      image: "/images/projects/ahmermalik.png",
       url: "github.com/projecturl",
       summary:
         "This project leverages JavaScript, Google Analytics, and jQuery to provide an exceptional user experience while boosting the restaurant's delivery service.",
@@ -126,7 +127,7 @@ const Portfolio: React.FC = () => {
       name: "Water, Water Everywhere!",
       technologies: ["JavaScript", "Google Analytics", "jQuery"],
       description: "NASA.",
-      image: "/image/path",
+      image: "/images/projects/waterwater.png",
       url: "github.com/projecturl",
       summary: "Utilizing Watson's A.I. to detect floods in an image",
       keyFeatures: [
@@ -148,7 +149,7 @@ const Portfolio: React.FC = () => {
         "Enzyme",
       ],
       description: "Craigslist scrapper",
-      image: "/image/path",
+      image: "/images/projects/houstonhelps.png",
       url: "github.com/projecturl",
       summary: "Grabbed free stuff from Craigslist",
       keyFeatures: [
@@ -161,128 +162,193 @@ const Portfolio: React.FC = () => {
   const [activeProject, setActiveProject] = useState(projects[0]);
 
   return (
-    <div>
-      <Grid container spacing={2}>
-        {/* 1st Grid */}
-        <Grid
-          item
-          xs={12}
-          md={2}
-          sx={{ marginLeft: "10px", marginRight: "10px" }}
+    <Grid
+      container
+      spacing={2}
+      sx={{ marginTop: "40px", marginBottom: "40px" }}
+    >
+      {/* 1st Grid */}
+      <Grid
+        item
+        xs={12}
+        md={2}
+        sx={{ marginLeft: "10px", marginRight: "10px" }}
+      >
+        <Typography
+          variant="h2"
+          className={""}
+          sx={{ fontSize: "56px", marginBottom: "20px" }}
+        >
+          Portfolio
+        </Typography>
+        <Typography
+          variant="h4"
+          sx={{ fontSize: "26px", fontWeight: "600", marginBottom: "60px" }}
+        >
+          Explore my Digital Creations
+        </Typography>
+        <Box
+          sx={{
+            backgroundColor: "white",
+            borderRadius: "30px",
+            padding: "20px",
+          }}
         >
           <Typography
-            variant="h2"
-            className={""}
-            sx={{ fontSize: "56px", marginBottom: "20px" }}
+            variant="subtitle1"
+            sx={{ fontSize: "24px", fontWeight: "600" }}
           >
-            Portfolio
+            Projects
           </Typography>
+          <Box sx={{ marginBottom: "10px" }}>
+            {projects.map((project) => (
+              <Typography
+                key={project.name}
+                onClick={() => setActiveProject(project)}
+                sx={{
+                  fontSize: "20px",
+                  lineHeight: "30px",
+                  fontWeight: "400",
+                  cursor: "pointer",
+                  padding: "10px",
+                  backgroundColor:
+                    activeProject?.name === project.name
+                      ? "#FF9B501A"
+                      : "transparent",
+                  color:
+                    activeProject?.name === project.name
+                      ? "#FF9B50"
+                      : "#787878",
+                }}
+              >
+                {project.name}
+              </Typography>
+            ))}
+          </Box>
+
           <Typography
-            variant="h4"
-            sx={{ fontSize: "26px", fontWeight: "600", marginBottom: "60px" }}
+            variant="subtitle1"
+            sx={{ fontSize: "24px", fontWeight: "600", marginBottom: "20px" }}
           >
-            Explore my Digital Creations
+            Nonprofit Projects
           </Typography>
-          <Box
+          <Box>
+            {nonprofitProjects.map((project) => (
+              <Typography
+                key={project.name}
+                onClick={() => setActiveProject(project)}
+                sx={{
+                  fontSize: "20px",
+                  lineHeight: "30px",
+                  fontWeight: "400",
+                  cursor: "pointer",
+                  padding: "10px",
+                  backgroundColor:
+                    activeProject?.name === project.name
+                      ? "#FF9B501A"
+                      : "transparent",
+                  color:
+                    activeProject?.name === project.name
+                      ? "#FF9B50"
+                      : "#787878",
+                }}
+              >
+                {project.name}
+              </Typography>
+            ))}
+          </Box>
+        </Box>
+      </Grid>
+
+      {/* 2nd Grid */}
+      <Grid item xs={12} md={9}>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Typography
+            variant="h5"
             sx={{
-              backgroundColor: "white",
-              borderRadius: "30px",
-              padding: "20px",
+              color: "#2C2C2C",
+              fontSize: ["24px", "32px", "56px"],
+              fontStyle: "normal",
             }}
           >
-            <Typography
-              variant="subtitle1"
-              sx={{ fontSize: "24px", fontWeight: "600" }}
-            >
-              Projects
-            </Typography>
-            <Box sx={{ marginBottom: "10px" }}>
-              {projects.map((project) => (
-                <Typography
-                  key={project.name}
-                  onClick={() => setActiveProject(project)}
-                  sx={{
-                    fontSize: "20px",
-                    lineHeight: "30px",
-                    fontWeight: "400",
-                    cursor: "pointer",
-                    padding: "10px",
-                    backgroundColor:
-                      activeProject?.name === project.name
-                        ? "#FF9B501A"
-                        : "transparent",
-                    color:
-                      activeProject?.name === project.name
-                        ? "#FF9B50"
-                        : "#787878",
-                  }}
-                >
-                  {project.name}
-                </Typography>
-              ))}
-            </Box>
-
-            <Typography
-              variant="subtitle1"
-              sx={{ fontSize: "24px", fontWeight: "600", marginBottom: "20px" }}
-            >
-              Nonprofit Projects
-            </Typography>
-            <Box>
-              {nonprofitProjects.map((project) => (
-                <Typography
-                  key={project.name}
-                  onClick={() => setActiveProject(project)}
-                  sx={{
-                    fontSize: "20px",
-                    lineHeight: "30px",
-                    fontWeight: "400",
-                    cursor: "pointer",
-                    padding: "10px",
-                    backgroundColor:
-                      activeProject?.name === project.name
-                        ? "#FF9B501A"
-                        : "transparent",
-                    color:
-                      activeProject?.name === project.name
-                        ? "#FF9B50"
-                        : "#787878",
-                  }}
-                >
-                  {project.name}
-                </Typography>
-              ))}
-            </Box>
-          </Box>
-        </Grid>
-
-        {/* 2nd Grid */}
-        <Grid item xs={12} md={9}>
-          <Typography variant="h5">{activeProject.name}</Typography>
-          <ArrowButton btnName={"View Project"} />
-          <Typography variant="body1">
-            {activeProject.technologies.join(", ")}
+            {activeProject.name}
           </Typography>
-          <Typography variant="body2">{activeProject.description}</Typography>
-          <Image
-            src={activeProject.image}
-            alt={activeProject.name}
-            width="100"
-            height="100"
-            style={{ maxWidth: "600px" }}
-          />
-          <Typography variant="body1">{activeProject.summary}</Typography>
-          <Typography variant="body2">Key Features:</Typography>
-          <ul>
-            {activeProject.keyFeatures.map((feature) => (
-              <li key={feature}>
-                <Typography variant="body2">{feature}</Typography>
-              </li>
-            ))}
-          </ul>
-        </Grid>
+          <ArrowButton btnName={"View Project"} />
+        </Box>
+        <Typography
+          variant="body1"
+          sx={{
+            color: "#FF9B50",
+            fontSize: ["16px", "20px", "24px"],
+          }}
+        >
+          {activeProject.technologies.join(", ")}
+        </Typography>
+
+        <Typography
+          variant="body2"
+          sx={{
+            color: "#787878",
+            fontSize: ["15px", "17px", "18px"],
+            fontStyle: "normal",
+            fontWeight: 400,
+            lineHeight: "50px",
+          }}
+        >
+          {activeProject.description}
+        </Typography>
+
+        <Image
+          src={activeProject.image}
+          alt={activeProject.name}
+          layout="responsive" 
+          width={600}
+          height={600}
+          style={{
+            width: "100%", // Will take up the full width of its container up to 600px
+            maxWidth: "1000px",
+            maxHeight: "700px",
+            borderRadius: "20px",
+            background: `url(${activeProject.image}), lightgray -249.484px -6px / 144.156% 100.75% no-repeat`,
+            boxShadow: "0px 20px 40px 0px rgba(0, 0, 0, 0.10)",
+            marginBottom: "25px",
+          }}
+        />
+
+        <Typography
+          variant="body1"
+          sx={{
+            color: "#787878",
+            fontSize: ["15px", "17px", "18px"],
+          }}
+        >
+          {activeProject.summary}
+        </Typography>
+
+        <Typography variant="body2">Key Features:</Typography>
+        <ul>
+          {activeProject.keyFeatures.map((feature) => (
+            <li key={feature}>
+              <Typography
+                variant="body2"
+                sx={{
+                  color: "#787878",
+                  fontSize: ["15px", "17px", "18px"],
+                }}
+              >
+                {feature}
+              </Typography>
+            </li>
+          ))}
+        </ul>
       </Grid>
-    </div>
+    </Grid>
   );
 };
 
