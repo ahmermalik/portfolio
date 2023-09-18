931f0960e97f5b5131b108ec39d1d63bc8736927
Ahmer Malik <ahmermalik@Ahmers-Mac-mini.local>
2023-09-18 03:09:39 -0500

    remove model, activate animation

--- src/components/Astronaut.tsx
+++ src/components/Astronaut.tsx
@@ -1,31 +1,61 @@
 import React, { useState, useRef, useEffect } from "react";
-import { Canvas, useThree } from "@react-three/fiber";
+import * as THREE from "three";
+import { Canvas, useThree, useFrame } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import Box from "@mui/material/Box";
-import * as THREE from "three";
-import { PerspectiveCamera, OrbitControls } from "@react-three/drei";
+import { PerspectiveCamera } from "@react-three/drei";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-const SceneSetup: React.FC = () => {
-  const { camera, gl, scene } = useThree();
+const AnimatedModel = () => {
+  const { scene } = useThree();
+  const mixerRef = useRef(null);
 
   useEffect(() => {
     const loader = new GLTFLoader();
-    loader.load("/astro/scene.gltf", (gltf) => {
+    loader.load('/astro/scene.gltf', (gltf) => {
       const model = gltf.scene;
-      if (model) {
-        model.position.set(0, 0, 0);
-        model.scale.set(55, 55, 55);
-        scene.add(model);
-      } else {
-        console.error("Model not loaded or parsed correctly");
+      model.position.set(0, 0, 0);
+      model.scale.set(55, 55, 55);
+      scene.add(model);
+
+      if (gltf.animations && gltf.animations.length > 0) {
+        const mixer = new THREE.AnimationMixer(model);
+        gltf.animations.forEach((clip) => {
+          mixer.clipAction(clip).play();
+        });
+        mixerRef.current = mixer;
       }
     });
   }, [scene]);
 
+  useFrame((state, delta) => {
+    if (mixerRef.current) mixerRef.current.update(delta);
+  });
+
+  return null;
+};
+
+const SceneSetup: React.FC = () => {
+  const { camera, gl } = useThree();
+  const controlsRef = useRef(null);
+
+  useEffect(() => {
+    controlsRef.current = new OrbitControls(camera, gl.domElement);
+    controlsRef.current.autoRotate = false;
+    controlsRef.current.autoRotateSpeed = 0.5;
+    return () => controlsRef.current.dispose();
+  }, [camera, gl]);
+
+  useFrame(() => {
+    if (controlsRef.current) {
+      controlsRef.current.update();
+    }
+  });
+
   return (
     <>
-      <OrbitControls autoRotate={false} autoRotateSpeed={0.5} />
-      <PerspectiveCamera makeDefault position={[20, 250, 190]} />
+      <AnimatedModel />
+      <PerspectiveCamera makeDefault position={[20, 250, 290]} />
       <ambientLight intensity={0.7} />
       <directionalLight position={[10, 50, 30]} intensity={1.5} />
       <directionalLight position={[-10, 50, -30]} intensity={1.5} />
@@ -40,22 +70,18 @@ const Astronaut: React.FC = () => {
   useEffect(() => {
     const handleResize = () => {
       if (boxRef.current) {
-        // Set the dimensions based on the container size
         const width = boxRef.current.clientWidth;
         const height = boxRef.current.clientHeight;
-
-        // You can perform any rescaling based on these dimensions if necessary.
       }
     };
-
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
-    <Box ref={boxRef} sx={{ height: "900px", width: "1500px" }}>
+    <Box component={"div"} ref={boxRef} sx={{ height: "900px", width: "1500px" }}>
       <Canvas onCreated={({ gl }) => {
-        gl.outputEncoding = THREE.sRGBEncoding;
+        gl.outputColorSpace = THREE.outputColorSpace;
         setLoading(false);
       }}>
         <SceneSetup />
