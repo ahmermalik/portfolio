43232476de4a533d0269087d0af8b7302f85e18c
Ahmer Malik <ahmermalik@Ahmers-Mac-mini.local>
2023-09-21 18:17:16 -0500

    portoflio completed, maybe i'll do a contact section, but idk

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -25,7 +25,11 @@ const TestimonialCard: React.FC<TestimonialCardProps> = ({ testimonial }) => {
   const remaining = testimonial.statement.slice(150);
 
   return (
-    <Box component={"div"} sx={{ minWidth: 300, padding: 2, marginRight: 3 }} className={styles.tcard}>
+    <Box
+      component={"div"}
+      sx={{ minWidth: 300, padding: 2, marginRight: 3 }}
+      className={styles.tcard}
+    >
       <Avatar
         src={testimonial.photo}
         alt={testimonial.name}
@@ -34,7 +38,11 @@ const TestimonialCard: React.FC<TestimonialCardProps> = ({ testimonial }) => {
       <Typography component={"p"} variant="h6" className={styles.cardName}>
         {testimonial.name}
       </Typography>
-      <Typography component={"p"} variant="body2" className={styles.cardTestimony}>
+      <Typography
+        component={"p"}
+        variant="body2"
+        className={styles.cardTestimony}
+      >
         {snippet}
         <Collapse in={expanded}>{remaining}</Collapse>
         {remaining && (
@@ -63,23 +71,25 @@ const Testimonials: React.FC = () => {
     scrollRef.current?.scrollBy({
       top: 0,
       left: scrollAmount,
-      behavior: "smooth"
+      behavior: "smooth",
     });
   };
 
   return (
     <Box component={"div"} className={styles.testimonials}>
-      <Typography variant="h4" sx={{ fontSize: ["35px", "50px"]}} className={styles.testitle}>
+      <Typography
+        variant="h4"
+        sx={{ fontSize: ["35px", "50px"] }}
+        className={styles.testitle}
+      >
         See What Clients and Collaborators Have to Say About My Work
       </Typography>
 
-      <Box  className={styles.iconLeft} component={"div"} >
-
-      <IconButton onClick={() => scroll("left")}>
-        <ChevronLeft />
-      </IconButton>
+      <Box className={styles.iconLeft} component={"div"}>
+        <IconButton onClick={() => scroll("left")}>
+          <ChevronLeft />
+        </IconButton>
       </Box>
-
 
       <Box ref={scrollRef} component={"div"} className={styles.testicard}>
         {testimonials.map((t) => (
@@ -87,11 +97,11 @@ const Testimonials: React.FC = () => {
         ))}
       </Box>
 
-     <Box className={styles.iconRight} component={"div"}>
-     <IconButton  onClick={() => scroll("right")}>
-        <ChevronRight />
-      </IconButton>
-     </Box>
+      <Box className={styles.iconRight} component={"div"}>
+        <IconButton onClick={() => scroll("right")}>
+          <ChevronRight />
+        </IconButton>
+      </Box>
     </Box>
   );
 };
